3a598d723c33c65de8b2e8b489c001df08f8d48c
RuchitaGanurkar <rmganukar1323@gmail.com>
2025-02-14 13:55:12 +0530

    Initial Generated Files Added

---
+++ Angular/basic-application/src/app/app.component.ts
@@ -0,0 +1,22 @@
+
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from './core/services/auth.service';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+
+
+export class AppComponent {
+  constructor(public authService: AuthService) {}
+
+  logout() {
+    this.authService.logout();
+  }
+}
